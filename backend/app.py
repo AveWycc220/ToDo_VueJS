5fcb38632aa8c8ec62f98dd0a7910fe786967835
AveWycc220 <mik16748@gmail.com>
2020-11-07 02:44:45 +0300

    feat (backend/app): add methods for login and update

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,35 @@
-from flask import Flask
+""" Module for Flask Application. """
+from flask import Flask, jsonify, request, make_response, abort
+from src.json_parser import JSONParser
 
 app = Flask(__name__)
+json_parser = JSONParser()
+app.config['JSON_AS_ASCII'] = False
+config = {
+    'DEBUG': True,
+    'PORT': 8080,
+}
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/login/<name>', methods=['GET'])
+def login(name):
+    """ Method for getting json for todo"""
+    response = make_response(jsonify(json_parser.get_json(name)))
+    response.set_cookie('name', name)
+    return response
+
+
+@app.route('/update/', methods=['POST'])
+def update():
+    """ Method for upload new information on server """
+    new_json = request.json
+    name = request.cookies.get('name')
+    if not name:
+        abort(401, description='Unauthorized')
+    else:
+        json_parser.update_json(name, new_json)
+        return jsonify({'status': 200})
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=config['PORT'], debug=config['DEBUG'])
