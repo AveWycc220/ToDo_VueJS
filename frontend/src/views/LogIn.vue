de2a8565c843b987099cb3ee8db8ba87b97585ef
AveWycc220 <mik16748@gmail.com>
2020-11-29 18:19:23 +0300

    feat (frontend/views): add view for LogIn

---
+++ frontend/src/views/LogIn.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="log-in">
+        <LogInHeader></LogInHeader>
+        <LogInForm />
+    </div>
+</template>
+
+<script>
+
+import LogInHeader from "@/components/LogInHeader"
+import LogInForm from "@/components/LogInForm"
+
+  export default {
+    name: "LogIn",
+    components: {
+      LogInHeader, LogInForm
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
