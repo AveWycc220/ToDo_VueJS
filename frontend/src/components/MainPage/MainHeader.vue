0ee8616b696e28e2c06f332dbf4ad9cb07270a26
AveWycc220 <mik16748@gmail.com>
2020-12-02 17:39:13 +0300

    feat (frontend/components): add Header for MainPage

---
+++ frontend/src/components/MainPage/MainHeader.vue
@@ -0,0 +1,47 @@
+<template>
+    <header>
+        <img src="@/assets/todo-icon.png" alt="todo-icon" id="logo-icon">
+    </header>
+</template>
+
+
+<script>
+  export default {
+    name: "MainHeader",
+  }
+</script>
+
+<style scoped>
+    header {
+        font-size: 22px;
+        display: flex;
+        background-color: #6564DB;
+        width: 100%;
+        height: 3.55em;
+    }
+
+    @media (min-width: 2000px) {
+        header {
+            font-size: 26px;
+        }
+    }
+
+    @media (min-width: 2300px) {
+        header {
+            font-size: 28px;
+        }
+    }
+
+    #logo {
+        font-family: 'Poppins', sans-serif;
+        margin-left: 0.625em;
+        margin-top: 0.925em;
+    }
+
+    #logo-icon {
+        margin-left: 0.625em;
+        margin-top: 0.625em;
+        width: 2em;
+        height: 2em;
+    }
+</style>
