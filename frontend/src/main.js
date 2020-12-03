e4196a946691e8d5c4b7242ddf39ec9bc2ccd403
AveWycc220 <mik16748@gmail.com>
2020-12-03 14:08:41 +0300

    feat (frontend/main): add plugin vue-cookie

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,8 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from "vue-router";
 import router from "@/router/router";
+import VueCookie from 'vue-cookie';
 
 Vue.use(VueRouter)
+Vue.use(VueCookie)
 
 new Vue({
   render: h => h(App),
